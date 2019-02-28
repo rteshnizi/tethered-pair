08a88b75c6df04c8e5cf05c68a380f643517009a
Reza Teshnizi <rht1369@yahoo.com>
2019-02-28 01:37:37 -0600

    feat(package.json): Remove usage of react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { HashRouter as Router, Link, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import { BindMemberMethods } from './utils/react';
 import { Home } from './home/home';
 
@@ -18,23 +17,9 @@ class App extends React.Component<{}, AppState> {
 
 	render() {
 		return (
-			<Router>
-				<div className="App">
-					<Switch>
-						<Route exact={true} path="/" component={Home} />
-						<Route render={this.render404} />
-					</Switch>
-				</div>
-			</Router>
-		);
-	}
-
-	private render404(props: RouteComponentProps): React.ReactNode {
-		return (
-			<div className="App-header">
-				Page Not Found
-				<Link className="App-header" to="/">Home</Link>
-			</div>
+		<div className="App">
+			<Home />
+		</div>
 		);
 	}
 }
