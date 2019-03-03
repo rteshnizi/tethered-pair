58cc5ec6755e13d18b58f8215694fb95993f48ab
Reza Teshnizi <rht1369@yahoo.com>
2019-03-03 17:00:56 -0600

    feat(InputForm): Add Obstacle Form

--- src/ui/InputArea.tsx
+++ src/ui/InputArea.tsx
@@ -4,7 +4,6 @@ import { fabric } from 'fabric';
 import * as React from 'react';
 import { AppSteps } from '../App';
 import { BindMemberMethods } from '../utils/react';
-import { StrPoint } from './geometry';
 import { Robot } from '../model/robot';
 import Model from '../model/model-service';
 import { trim } from 'lodash';
@@ -13,16 +12,20 @@ interface InputAreaProps {
 	activeStep: keyof AppSteps;
 }
 
-interface InputState {
-	robots: string[];
+class InputState {
+	public robots: string[];
+	public obstacles: string[][];
+
+	constructor() {
+		this.robots = ["0, 0", "0, 0"];
+		this.obstacles = [];
+	}
 }
 
 export class InputArea extends React.Component<InputAreaProps, InputState> {
 	constructor(props: InputAreaProps) {
 		super(props);
-		this.state = {
-			robots: ["0, 0", "0, 0"],
-		};
+		this.state = new InputState();
 		BindMemberMethods(InputArea.prototype, this);
 	}
 
@@ -51,7 +54,7 @@ export class InputArea extends React.Component<InputAreaProps, InputState> {
 						label={`Robot ${ind}`}
 						value={this.state.robots[ind - 1]}
 						margin="dense"
-						onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => { this.handleRobotChange(e.target.value, ind); }}
+						onChange={(e) => { this.handleRobotChange(e.target.value, ind); }}
 						/>
 				</Mui.Tooltip>
 				<Mui.IconButton className="inline-button" color="primary" aria-label="Add" onClick={() => { this.setRobot(ind); }}>
@@ -61,19 +64,62 @@ export class InputArea extends React.Component<InputAreaProps, InputState> {
 		);
 	}
 
+	addPointToObstacle(obsInd: number): void {
+		const currentObs = this.state.obstacles;
+		currentObs[obsInd].push("0, 0");
+		this.setState({ obstacles: currentObs });
+	}
+
+	updatePointInObstacle(val: string, obsInd: number, verInd: number): void {
+		const currentObs = this.state.obstacles;
+		currentObs[obsInd].push("0, 0");
+		this.setState({ obstacles: currentObs });
+	}
+
+	createObstaclePointInputs(obstacle: string[], obsInd: number): JSX.Element {
+		return (
+			<div>
+				<p>{`Obstacle${obsInd + 1}`}</p>
+				{obstacle.map((val: string, vertInd: number) => (
+					<Mui.Tooltip title="Comma separated X, Y" placement="top">
+						<Mui.TextField
+							className="point-input"
+							label={`V${vertInd + 1}`}
+							value={val}
+							margin="dense"
+							onChange={(e) => { this.updatePointInObstacle(e.target.value, obsInd, vertInd) }}
+							/>
+					</Mui.Tooltip>
+				))}
+				<Mui.Button className="inline-button" color="primary" aria-label="Add Vertex" onClick={() => { this.addPointToObstacle(obsInd) }}>
+					Add Vertex
+				</Mui.Button>
+				<Mui.IconButton className="inline-button" color="primary" aria-label="Add" onClick={() => { }}>
+					<RightArrowIcon fontSize="small" />
+				</Mui.IconButton>
+			</div>
+		);
+	}
+
 	createRowsForAddedObstacles(): JSX.Element {
 		return (
 			<div>
-				HELLO
+				{this.state.obstacles.map(this.createObstaclePointInputs)}
 			</div>
 		);
+	}
+
+	addObstacleToState(): void {
+		const currentObs = this.state.obstacles;
+		currentObs.push(["0, 0", "0, 0", "0, 0"]); // min obstacle is triangle, we only work with polygonal obstacles
+		this.setState({ obstacles: currentObs });
 	}
 
 	createObstacleInput(): JSX.Element {
 		return (
 			<div>
 				{this.createRowsForAddedObstacles()}
-				<Mui.Button variant="contained" color="primary" size="small">Create New Obstacle</Mui.Button>
+				<Mui.Button variant="contained" color="primary" size="small" onClick={this.addObstacleToState}>Create New Obstacle</Mui.Button>
 			</div>
 		);
 	}
