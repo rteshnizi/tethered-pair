f5d24c7d21ca331359bea676f76d1ffdca9ebd40
Reza Teshnizi <rht1369@yahoo.com>
2019-03-03 19:29:47 -0600

    feat(viewer): Canvas fills the screen

--- src/viewer/Viewer.tsx
+++ src/viewer/Viewer.tsx
@@ -5,18 +5,38 @@ import Renderer from './renderer-service';
 
 export const VIEWIER_CANVAS_ID = "viewer-canvas";
 
-export class Viewer extends React.Component {
+interface ViewerState {
+	width: number;
+	height: number;
+}
+
+export class Viewer extends React.Component<{}, ViewerState> {
 	constructor(props: {}) {
 		super(props);
+		this.state = {
+			height: 600,
+			width: 400,
+		};
 		BindMemberMethods(Viewer.prototype, this);
-		window.setTimeout(() => { Renderer.Instance.render(); }, 400);	// Maybe remove this later and see if it affects anything.
-																		// Right now if I don't do this the canvas won't initialize correctly
+		// Maybe remove this later and see if it affects anything.
+		// Right now if I don't do this the canvas won't initialize correctly
+		window.setTimeout(() => {
+			const el = document.getElementsByClassName("viewer")[0];
+			let width = 0;
+			let height = 0;
+			if (el) {
+				width = el.clientWidth;
+				height = el.clientHeight;
+				this.setState({ width, height });
+				window.setTimeout(() => { Renderer.Instance.render(); }, 10);
+			}
+		}, 250);
 	}
 
 	render() {
 		return (
 			<div className="viewer">
-				<canvas id={VIEWIER_CANVAS_ID} width="400px" height="600px" />
+				<canvas id={VIEWIER_CANVAS_ID} width={`${this.state.width}px`} height={`${this.state.height}px`} />
 				{/* <Button variant="contained" color="primary" onClick={() => { Renderer.Instance.render(); }}>Render</Button> */}
 			</div>
 		);
