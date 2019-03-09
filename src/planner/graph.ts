c51dbf267e7b9c8dbd9af534c395096369fe9b1e
Reza Teshnizi <rht1369@yahoo.com>
2019-03-09 16:36:52 -0600

    feat(graph): Add implementation
    It's not used however

--- src/planner/graph.ts
+++ src/planner/graph.ts
@@ -1,5 +1,35 @@
-import { fabric } from 'fabric';
+import { Vertex } from '../model/vertex';
 
 export class VisibilityGraph {
+	public edges: Map<Vertex, Set<Vertex>>;
+	constructor(public vertices: Vertex[]) {
+		this.edges = new Map();
+		this.buildEdges();
+	}
 
+	private buildEdges(): void {
+		for (let i = 0; i < this.vertices.length; i++) {
+			const v1 = this.vertices[i];
+			for (let j = i + 1; j < this.vertices.length; j++) {
+				const v2 = this.vertices[j];
+				if (v1.isVisible(v2)) {
+					this.makeSetIfNeeded(v1);
+					this.makeSetIfNeeded(v2);
+					this.edges.get(v1)!.add(v2);
+					this.edges.get(v2)!.add(v1);
+				}
+			}
+		}
+	}
+
+	private makeSetIfNeeded(v: Vertex): void {
+		if (this.edges.has(v)) return;
+		this.edges.set(v, new Set());
+	}
+
+	public hasEdge(v1: Vertex, v2: Vertex): boolean {
+		if (!this.edges.has(v1)) return false;
+		if (this.edges.get(v1)!.has(v2)) return true;
+		return false;
+	}
 }
