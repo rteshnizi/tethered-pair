6bc110d96581822191f53d0d76e2d93c30ed358f
Reza Teshnizi <rht1369@yahoo.com>
2019-03-10 19:27:17 -0500

    feat(obstacle): Add isMyEdge()

--- src/model/obstacle.ts
+++ src/model/obstacle.ts
@@ -1,6 +1,7 @@
 import { fabric } from 'fabric';
 import { Entity } from './entity';
 import { Vertex } from './vertex';
+import { Edge } from './edge';
 
 type Vertices = Vertex[];
 
@@ -8,17 +9,23 @@ const VERTEX_COLOR = "purple";
 const VERTEX_RADIUS = 3;
 const COLOR = "grey";
 
+/**
+ * Assumes the vertices passed in are sorted
+ * TODO: sort vertices inside the ctor instead of the viewier
+ */
 export class Obstacle extends Entity {
 	public vertices: Vertices;
+	// public edges: Edge[];
 	// TODO: Build list of edges as well -> Use Geometry function
 	constructor(name: string, public fabricPoints: fabric.Point[]/*, public left: number, public top: number*/) {
 		super(name, COLOR, new fabric.Polygon(fabricPoints, {
 			fill: COLOR
 		}), true);
 		this.vertices = this.createVertices(fabricPoints);
+		// this.edges = this.createEdges();
 	}
 
-	public createVertices(fabricPoints: fabric.Point[]): Vertices {
+	private createVertices(fabricPoints: fabric.Point[]): Vertices {
 		const verts: Vertices = [];
 		let i = 1;
 		fabricPoints.forEach((p) => {
@@ -29,5 +36,28 @@ export class Obstacle extends Entity {
 			}));
 		});
 		return verts;
+	}
+
+	// private createEdges(): Edge[] {
+	// 	const edges: Edge[] = [];
+	// 	for (let i = 1; i < this.vertices.length; i++) {
+	// 		const v1 = this.vertices[i - 1];
+	// 		const v2 = this.vertices[i];
+	// 		edges.push(new Edge(`${this.name}-${v1.name}${v2.name}`, v1, v2));
+	// 	}
+	// 	return edges;
+	// }
+
+	/** Checks whether this edge lies on the boundary of the obstacle */
+	// We don't need to calculate the edges for this because the list of vertices are sorted.
+	// TODO: If we cache the edges using a map this can have performance enhancement
+	public isMyEdge(edge: Edge): boolean {
+		for (let i = 1; i < this.vertices.length; i++) {
+			const v1 = this.vertices[i - 1];
+			const v2 = this.vertices[i];
+			if (edge.v1.location.eq(v1.location) && edge.v2.location.eq(v2.location)) return true;
+			if (edge.v2.location.eq(v1.location) && edge.v1.location.eq(v2.location)) return true;
+		}
+		return false;
 	}
 }
