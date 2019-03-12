d1f2b0ef22f5d64da26d82b3340e79040bcc738b
Reza Teshnizi <rht1369@yahoo.com>
2019-03-12 18:19:05 -0500

    feat(presets): Add more presets

--- src/model/presets.ts
+++ src/model/presets.ts
@@ -3,14 +3,24 @@ interface Preset {
 	json: string;
 }
 
-export const PresetUnset: Preset = {
+const PresetUnset: Preset = {
 	name: "Unset",
 	json: '{"robots":["0, 0","0, 0"],"destinations":["0, 0","0, 0"],"obstacles":[],"cableLength":0}',
 }
 
-export const Preset1: Preset = {
+const Preset1: Preset = {
 	name: "Preset 1",
 	json: '{"robots":["100, 50","500, 50"],"destinations":["150, 500", "330, 520"], "obstacles":[["175, 100","225, 100","225, 150","175, 150"],["300, 200","425, 200","300, 225","425, 225"],["50, 300","50, 350","250, 300","250, 350"],["325, 400","350, 400","325, 425","350, 425"],["425, 410","400, 435","450, 435"]],"cableLength":500}',
 }
 
-export const AllPresets = [PresetUnset, Preset1];
+const Preset2: Preset = {
+	name: "Preset 2",
+	json: '{"robots":["100, 50","500, 50"],"destinations":["370, 280", "270, 180"], "obstacles":[["175, 100","225, 100","225, 150","175, 150"],["300, 200","425, 200","300, 225","425, 225"],["50, 300","50, 350","250, 300","250, 350"],["325, 400","350, 400","325, 425","350, 425"],["425, 410","400, 435","450, 435"]],"cableLength":500}',
+}
+
+const Preset3: Preset = {
+	name: "Preset 3",
+	json: '{"robots":["100, 50","500, 50"],"destinations":["370, 280", "250, 220"], "obstacles":[["175, 100","225, 100","225, 150","175, 150"],["300, 200","425, 200","300, 225","425, 225"],["50, 300","50, 350","250, 300","250, 350"],["325, 400","350, 400","325, 425","350, 425"],["425, 410","400, 435","450, 435"]],"cableLength":500}',
+}
+
+export const AllPresets = [PresetUnset, Preset1, Preset2, Preset3];
